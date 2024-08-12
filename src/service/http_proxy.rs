5d27b2ea89b8ffd2ac2ca4227c66ba5cad9a33df
Alex Wellnitz <moin@wellnitz-alex.de>
2024-08-12 20:02:07 +0200

    feat: update dependencies and pingora versions to 0.3

--- src/service/http_proxy.rs
+++ src/service/http_proxy.rs
@@ -68,8 +68,8 @@ impl HttpProxy {
         let mut my_server = Server::new(None).unwrap();
         my_server.bootstrap();
 
+        // This has to be dynamic over the ingress configs
         let upstreams = LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443"]).unwrap();
-
         let mut lb = http_proxy_service(&my_server.configuration, LB(Arc::new(upstreams)));
         lb.add_tcp("0.0.0.0:6188");
 
